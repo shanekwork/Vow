b3d49456f6f8be38227d9217326c7f9f4718dc1d
Shane Kelly <shanekweb@icloud.com>
2016-04-07 09:34:37 +0100

    products controller image name to url

--- app/controllers/spree/products_controller.rb
+++ app/controllers/spree/products_controller.rb
@@ -20,7 +20,7 @@ module Spree
 
 
     def show
-	   @imageProds = "http://www.op-insight.co.uk/images/product/large/" + @product.image_name + ".jpg"
+	   @imageProds = "http://www.op-insight.co.uk/images/product/large/" + @product.image_url + ".jpg"
       @variants = @product.variants_including_master.active(current_currency).includes([:option_values, :images])
       @product_properties = @product.product_properties.includes(:property)
       @taxon = Spree::Taxon.find(params[:taxon_id]) if params[:taxon_id]
